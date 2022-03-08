315845412dc5f186abe4cccff26804dbd193d948
Liliane Soares <78764415+soareslil@users.noreply.github.com>
2022-03-08 11:43:39 -0300

    adicionando listadecategorias and more

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import ListaDeNotas from "./components/ListaDeNotas";
 import FormularioCadastro from "./components/FormularioCadastro";
+import ListaDeCategorias from "./components/ListaDeCategorias";
 import "./assets/App.css";
 import './assets/index.css';
 class App extends Component {
@@ -33,10 +34,14 @@ class App extends Component {
     return (
       <section className="conteudo">
         <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
-        <ListaDeNotas
+        <main className="conteudo-principal">
+          <ListaDeCategorias />
+
+          <ListaDeNotas
          notas={this.state.notas}
          apagarNota = {this.deletarNota.bind(this)}
          />
+        </main>
       </section>
     );
   }
