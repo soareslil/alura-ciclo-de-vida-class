a7f2604ccccd69082fd957293b5267a959a59a5e
Liliane Soares <78764415+soareslil@users.noreply.github.com>
2022-09-14 21:14:24 -0300

    pushing changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
 
     // this.state = {
     //   notas:[],
-    //   // categorias: [],
+    //   categorias: [],
     // }
   }
 
@@ -46,17 +46,17 @@ class App extends Component {
     return (
       <section className="conteudo">
         <FormularioCadastro 
-        categorias={this.categorias.categorias}
-        criarNota={this.notas._criarNota}
+        categorias={this.categorias}
+        criarNota={this.notas.adicionarNota.bind(this.notas)}
         />
         <main className="conteudo-principal">
           <ListaDeCategorias
-          adicionarCategoria = {this.categorias.adicionarCategoria}
-          categorias={this.categorias.categorias} />
+          adicionarCategoria = {this.categorias.adicionarCategoria.bind(this.categorias)}
+          categorias={this.categorias} />
 
           <ListaDeNotas
-         notas={this.notas.notas}
-         apagarNota = {this.notas.apagarNota}
+         notas={this.notas}
+         apagarNota = {this.notas.apagarNota.bind(this.notas)}
          />
         </main>
       </section>
--- src/dados/Notas.js
+++ src/dados/Notas.js
@@ -1,17 +1,32 @@
 export default class ArrayDeNotas{
     constructor(){
         this.notas = [];
+        this._inscritos = [];
     }
 
     adicionarNota(titulo, texto, categoria){
         const novaNota = new Nota(titulo, texto, categoria);
-        this.notas.push(novaNota)
+        this.notas.push(novaNota);
+        this.notificar();
     }
 
     apagarNota(indice){
         this.notas.splice(indice, 1);
+        this.notificar();
     }
 
+    inscrever(func){
+        this._inscritos.push(func);
+    }
+    desinscrever(func){
+        this._inscritos = this._inscritos.filter(f => f !== func);
+    }
+
+    notificar(){
+        this._inscritos.forEach(func =>{
+            func(this.notas);
+        } );
+    }
 }
 
 class Nota{
@@ -20,5 +35,4 @@ class Nota{
         this.texto = texto;
         this.categoria = categoria;
     }
-
 }
